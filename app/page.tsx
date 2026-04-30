ba75f4252002e070992594eba62e1aa37b535192
Evrlink <evrlinkmeeps@gmail.com>
2026-04-29 17:59:27 -0700

    force no store on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from 'next'
 import Gallery from './gallery'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+
 export const metadata: Metadata = {
   other: {
     'base:app_id': '69f2a19c6daaf9236cfba3d9',
