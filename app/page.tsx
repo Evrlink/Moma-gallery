1d39839d7518c86801a85a2c27adf32a23a930c9
Evrlink <evrlinkmeeps@gmail.com>
2026-04-29 17:14:58 -0700

    force dynamic no cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next'
 import Gallery from './gallery'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export const metadata: Metadata = {
   other: {
     'base:app_id': '69f185d25db18b50b3cf003a',
