5b1a93fb70da198097682c2d2728a9e4ea9acdbd
Evrlink <evrlinkmeeps@gmail.com>
2026-04-29 17:32:51 -0700

    update page.tsx with new base app id

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import { Metadata } from 'next'
 import Gallery from './gallery'
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
 export const metadata: Metadata = {
   other: {
     'base:app_id': '69f2a19c6daaf9236cfba3d9',
